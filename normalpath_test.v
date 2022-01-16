70395dd0d6f41fadf52376b66ba9c103c9df7f97
Subhomoy Haldar <hungrybluedev@gmail.com>
2022-01-16 12:11:11 +0530

    run vfmt; use main module

--- normalpath_test.v
+++ normalpath_test.v
@@ -1,13 +1,14 @@
-module normalpath
+module main
+
+import normalpath { chain_path, normalize_path }
 
 fn test_chain_path() {
-	assert chain_path("foo", "bar", "baz") == "foo/bar/baz"
-	assert chain_path("/foo/", "bar/baz") == "/foo/bar/baz"
-	assert chain_path("/foo", "/bar") == "/bar"
+	assert chain_path('foo', 'bar', 'baz') == 'foo/bar/baz'
+	assert chain_path('/foo/', 'bar/baz') == '/foo/bar/baz'
+	assert chain_path('/foo', '/bar') == '/bar'
 }
 
-fn test_normalize_path()
-{
+fn test_normalize_path() {
 	assert normalize_path('/foo/./bar/..//baz/') == '/foo/baz'
 	assert normalize_path('../foo/.') == '../foo'
 	assert normalize_path('/foo', 'bar/baz/') == '/foo/bar/baz'
@@ -15,17 +16,17 @@ fn test_normalize_path()
 	assert normalize_path('foo/./bar', '../../', '../baz') == '../baz'
 	assert normalize_path('/foo/./bar', '../../baz') == '/baz'
 
-	assert normalize_path("/", "foo", "bar") == '/foo/bar'
-	assert normalize_path("foo", "bar", "baz") == "foo/bar/baz"
-    assert normalize_path("foo", "bar/baz") == "foo/bar/baz"
-    assert normalize_path("foo", "bar//baz///") == "foo/bar/baz"
-    assert normalize_path("/foo", "bar/baz") == "/foo/bar/baz"
-    assert normalize_path("/foo", "/bar/baz") == "/bar/baz"
-    assert normalize_path("/foo/..", "/bar/./baz") == "/bar/baz"
-    assert normalize_path("/foo/..", "bar/baz") == "/bar/baz"
-    assert normalize_path("/foo/../../", "bar/baz") == "/bar/baz"
-    assert normalize_path("/foo/bar", "../baz") == "/foo/baz"
-    assert normalize_path("/foo/bar", "../../baz") == "/baz"
-    assert normalize_path("/foo/bar", ".././/baz/..", "wee/") == "/foo/wee"
-    assert normalize_path("//foo/bar", "baz///wee") == "/foo/bar/baz/wee"
+	assert normalize_path('/', 'foo', 'bar') == '/foo/bar'
+	assert normalize_path('foo', 'bar', 'baz') == 'foo/bar/baz'
+	assert normalize_path('foo', 'bar/baz') == 'foo/bar/baz'
+	assert normalize_path('foo', 'bar//baz///') == 'foo/bar/baz'
+	assert normalize_path('/foo', 'bar/baz') == '/foo/bar/baz'
+	assert normalize_path('/foo', '/bar/baz') == '/bar/baz'
+	assert normalize_path('/foo/..', '/bar/./baz') == '/bar/baz'
+	assert normalize_path('/foo/..', 'bar/baz') == '/bar/baz'
+	assert normalize_path('/foo/../../', 'bar/baz') == '/bar/baz'
+	assert normalize_path('/foo/bar', '../baz') == '/foo/baz'
+	assert normalize_path('/foo/bar', '../../baz') == '/baz'
+	assert normalize_path('/foo/bar', '.././/baz/..', 'wee/') == '/foo/wee'
+	assert normalize_path('//foo/bar', 'baz///wee') == '/foo/bar/baz/wee'
 }
